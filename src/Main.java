7260f13ec304220cecb8264e7ebe712faf518e17
Рустам <rustam19871203@gmail.com>
2022-09-30 14:46:37 +0400

    Домашнее задание 3 Задание 7

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        var boxerMaxWeight = 78.2;
+        var boxerBobWeight = 82.7;
+        var differenceWeight = boxerBobWeight - boxerMaxWeight;
+        System.out.println(differenceWeight);
+        var remainsWeight = boxerBobWeight % boxerMaxWeight;
+        System.out.println(remainsWeight);
+
+    }
+}
