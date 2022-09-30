a824d1bccf968812d39520f19cc744ecb408a70d
Рустам <rustam19871203@gmail.com>
2022-09-30 15:32:39 +0400

    Домашнее задание 3 Задание 8.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        var totalWorkingHours = 640;
+        var workingHours = 8;
+        var totalNumberOfEmployees = totalWorkingHours / workingHours;
+        System.out.println(totalNumberOfEmployees);
+    }
+}
